7e8227dab3e419e1a1234b91976f798ec447c76d
adryp <adryp@DESKTOP-5KA3UJG>
2017-11-25 23:34:56 +0200

    Connect simulator to application.

--- src/sensors/simulator/SensorsSimulator.java
+++ src/sensors/simulator/SensorsSimulator.java
@@ -5,6 +5,17 @@
  */
 package sensors.simulator;
 
+import gui.SimulatorFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ASimionescu
@@ -16,6 +27,64 @@ public class SensorsSimulator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        SimulatorFrame gui = new SimulatorFrame();
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+               gui.setVisible(true);
+            }
+        });
+        
+        
+        try {
+            Socket s = new Socket("localhost", 60010);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+
+            gui.getBtn1().addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    sentMsg(out, "Btn1");
+                }
+            });
+            
+            gui.getBtn2().addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    sentMsg(out, "Btn2");
+                }
+            });
+            
+            gui.getBtn3().addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    sentMsg(out, "Btn3");
+                }
+            });
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
+    private static void sentMsg(BufferedWriter out, String msg)
+    {
+        try {
+            out.write(msg);
+            out.newLine();
+            out.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(SensorsSimulator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
